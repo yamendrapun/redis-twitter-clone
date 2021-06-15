42b5a667df221e367675984ab41b8f1a67e0e64f
Yamendra Pun <punyamendra@gmail.com>
2021-06-15 19:09:45 +0900

    Receive the login and password in Express

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
+
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => res.render('index'))
+
+app.post('/', (req, res) => {
+  const { username, password } = req.body
+
+  if (!username || !password) {
+    res.render('error', {
+      message: 'Please set both username and password',
+    })
+    return
+  }
+  console.log(req.body, username, password)
+  res.end()
+})
 
 app.listen(3000, () => console.log('server ready!'))
