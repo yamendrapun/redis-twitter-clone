8c7e5f31e0ca30129a1381f4c860a58d5732c3ea
Yamendra Pun <punyamendra@gmail.com>
2021-06-15 18:54:58 +0900

    Setup the Pug templating engine

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.get('/', (req, res) => res.send('hello world!'))
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(3000, () => console.log('server ready!'))
